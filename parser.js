494400290f27dcd440f170ead90812b9d7ce9542
vetsinen <vetsinen@gmail.com>
2022-01-28 11:16:16 +0200

    grab one page to json

--- parser.js
+++ parser.js
@@ -2,27 +2,41 @@ const axios = require('axios')
 const cheerio = require('cheerio')
 const fs = require('fs');
 
-
-const startUrl = 'https://100realty.ua/uk/realty_search/apartment/rent/cur_3/kch_2?page=0'
+const startUrl = 'https://100realty.ua/uk/realty_search/apartment/rent/cur_3/kch_2?page=' //0
+let rooms = []
 
 async function grab() {
-    //const html = await axios(startUrl)
-    const html = fs.readFileSync('./source.html',
-        {encoding: 'utf8', flag: 'r'});
-    const $ = cheerio.load(html)
-    $('.object-additional-info-wrapper').each((i, el) => {
+
+    //let html = await axios(startUrl)
+    const html = fs.readFileSync('./source.html',{encoding: 'utf8', flag: 'r'})
+
+    let $ = cheerio.load(html)
+
+    $('.realty-object-card').each((i, el) => {
         let room = {}
         room.adress = ($(el).find('.object-address a').text())
         room.quanity = parseFloat($(el).find('.object-rooms .value').text())
         let temp = $(el).find('.cost-field span').text().replace(/\s+/g, '')
         room.price = parseFloat(temp)
-        temp = $(el).find('.area a').text()
+        temp = $(el).find('.area a').first().text()
         room.mstation = temp
-        room.floor = $(el).find('.object-floors .value').text()
+        temp = $(el).find('.object-floors .value').text()
+        room.floor = parseFloat(temp)
+        room.totalFloors = parseFloat(temp.split('/')[1])
         room.square = parseFloat($(el).find('.object-square .value').text())
-        console.log(room)
+        room.district = $(el).find('.object-region a').first().text()
+        temp  = $(el).find('.descr').text().replace(/\n/g, '').replace(/\s+/g, " ")
+        room.description  = temp.trim()
+        temp = $(el).find('.object-address a').attr('href')
+        room.link = temp
+        room.id = temp.split('/')[3]
+        //room.img = $(el).find('.slick-track .slides__item').first().html()
+
+        rooms.push(room)
         }
     )
+    //console.log(rooms)
+    fs.writeFileSync("rooms.json", JSON.stringify(rooms))
 }
 
 grab()
---
+++ tmp.js
@@ -0,0 +1,7 @@
+// const fs = require('fs');
+// const rooms = fs.readFileSync('./rooms.json', {encoding: 'utf8', flag: 'r'})
+// console.log(JSON.parse(rooms)[0])
+
+var str = "Hello^# World/";
+str = str.replace(/[^a-zA-Z ]/g, "") // "Hello World"
+console.log(str)
