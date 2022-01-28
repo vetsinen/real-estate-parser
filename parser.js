19bd6c3ad2148c19d1ae6f22f111b28a8872545a
vetsinen <vetsinen@gmail.com>
2022-01-28 11:27:23 +0200

    surf multiple pages

--- parser.js
+++ parser.js
@@ -6,36 +6,36 @@ const startUrl = 'https://100realty.ua/uk/realty_search/apartment/rent/cur_3/kch
 let rooms = []
 
 async function grab() {
+    for (let i = 0; i < 100; i++) {
+        let html = await axios(startUrl + i)
+        //const html = fs.readFileSync('./source.html',{encoding: 'utf8', flag: 'r'})
+        let $ = cheerio.load(html.data)
 
-    //let html = await axios(startUrl)
-    const html = fs.readFileSync('./source.html',{encoding: 'utf8', flag: 'r'})
+        $('.realty-object-card').each((i, el) => {
+                let room = {}
+                room.adress = ($(el).find('.object-address a').text())
+                room.quanity = parseFloat($(el).find('.object-rooms .value').text())
+                let temp = $(el).find('.cost-field span').text().replace(/\s+/g, '')
+                room.price = parseFloat(temp)
+                temp = $(el).find('.area a').first().text()
+                room.mstation = temp
+                temp = $(el).find('.object-floors .value').text()
+                room.floor = parseFloat(temp)
+                room.totalFloors = parseFloat(temp.split('/')[1])
+                room.square = parseFloat($(el).find('.object-square .value').text())
+                room.district = $(el).find('.object-region a').first().text()
+                temp = $(el).find('.descr').text().replace(/\n/g, '').replace(/\s+/g, " ")
+                room.description = temp.trim()
+                temp = $(el).find('.object-address a').attr('href')
+                room.link = temp
+                room.id = temp.split('/')[3]
+                //room.img = $(el).find('.slick-track .slides__item').first().html()
 
-    let $ = cheerio.load(html)
-
-    $('.realty-object-card').each((i, el) => {
-        let room = {}
-        room.adress = ($(el).find('.object-address a').text())
-        room.quanity = parseFloat($(el).find('.object-rooms .value').text())
-        let temp = $(el).find('.cost-field span').text().replace(/\s+/g, '')
-        room.price = parseFloat(temp)
-        temp = $(el).find('.area a').first().text()
-        room.mstation = temp
-        temp = $(el).find('.object-floors .value').text()
-        room.floor = parseFloat(temp)
-        room.totalFloors = parseFloat(temp.split('/')[1])
-        room.square = parseFloat($(el).find('.object-square .value').text())
-        room.district = $(el).find('.object-region a').first().text()
-        temp  = $(el).find('.descr').text().replace(/\n/g, '').replace(/\s+/g, " ")
-        room.description  = temp.trim()
-        temp = $(el).find('.object-address a').attr('href')
-        room.link = temp
-        room.id = temp.split('/')[3]
-        //room.img = $(el).find('.slick-track .slides__item').first().html()
-
-        rooms.push(room)
-        }
-    )
-    //console.log(rooms)
+                rooms.push(room)
+                console.log(room)
+            }
+        )
+    }
     fs.writeFileSync("rooms.json", JSON.stringify(rooms))
 }
 
